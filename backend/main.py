1f9c04a882419d034e35b178a8f68c7ac9e3e301
Allan <allanhender@gmail.com>
2026-02-02 16:00:58 +0000

    updating main.py for deployment

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ def get_application() -> FastAPI:
     _app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
+        allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
     )
